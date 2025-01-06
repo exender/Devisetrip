afb9a4c57d4a735629a38c8355621f5cac876f7c
exender <39946425+exender@users.noreply.github.com>
2025-01-06 22:19:37 +0000

    Mise en place du docker

--- devisetrip/server/index.js
+++ devisetrip/server/index.js
@@ -1,44 +1,34 @@
 const express = require('express');
+const path = require('path');
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 const cors = require('cors');
-const userRoutes = require('./routes/userRoutes'); // Routes utilisateur
 
-// Charger les variables d'environnement
 dotenv.config();
 
 const app = express();
 
-// Middleware pour JSON et CORS
 app.use(express.json());
-app.use(
-  cors({
-    origin: '*', // Permettre toutes les origines (ou spécifiez une origine particulière)
-    methods: ['GET', 'POST', 'PUT', 'DELETE'],
-    allowedHeaders: ['Content-Type', 'Authorization'],
-  })
-);
+app.use(cors());
+
+// Middleware pour servir les fichiers statiques du frontend
+app.use(express.static(path.join(__dirname, 'public')));
 
 // Connexion à MongoDB
 mongoose
-  .connect(process.env.MONGO_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
+  .connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log('Connexion à MongoDB réussie'))
-  .catch((err) => {
-    console.error('Erreur de connexion MongoDB :', err);
-    process.exit(1); // Arrêter le serveur si la connexion échoue
-  });
+  .catch((err) => console.error('Erreur de connexion MongoDB :', err));
 
-// Route de test
-app.get('/', (req, res) => {
-  res.send('Backend opérationnel et connecté à MongoDB !');
-});
-
-// Utilisation des routes utilisateur
+// Routes
+const userRoutes = require('./routes/userRoutes');
 app.use('/api/users', userRoutes);
 
-// Démarrage du serveur
+// Route pour servir le frontend pour toutes les autres routes
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+// Démarrer le serveur
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Serveur en écoute sur le port ${PORT}`));
